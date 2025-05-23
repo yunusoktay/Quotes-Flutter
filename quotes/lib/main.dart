f31ebad936d5ce724b32374b6b67e33976f0a1af
Yunus Oktay <ynsokty.3@gmail.com>
2025-05-23 18:28:10 +0300

    feat: implement bottom navigation with home and favorites views

--- quotes/lib/main.dart
+++ quotes/lib/main.dart
@@ -17,8 +17,40 @@ class _MyAppState extends State<MyApp> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [const HomeView(), const FavoritesView()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blueGrey,
+          foregroundColor: Colors.white,
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(_selectedIndex == 0 ? "Alıntılar" : "Favoriler"),
+        ),
+        body: IndexedStack(index: _selectedIndex, children: _pages),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Alıntılar'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favoriler',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
   }
 }
