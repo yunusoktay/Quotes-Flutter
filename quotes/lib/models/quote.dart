7fad84c50154b0148f015840b74b35c4afb162e5
Yunus Oktay <ynsokty.3@gmail.com>
2025-05-23 18:39:07 +0300

    feat: define Quote data model

--- quotes/lib/models/quote.dart
+++ quotes/lib/models/quote.dart
@@ -1,1 +1,27 @@
+class Quote {
+  final int id;
+  final String quote;
+  final String author;
+  bool isFavorite;
 
+  Quote({
+    required this.id,
+    required this.quote,
+    required this.author,
+    this.isFavorite = false,
+  });
+
+  factory Quote.fromJson(Map<String, dynamic> json) => Quote(
+    id: json['id'] as int,
+    quote: json['quote'] as String,
+    author: json['author'] as String,
+    isFavorite: json['isFavorite'] as bool? ?? false,
+  );
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'quote': quote,
+    'author': author,
+    'isFavorite': isFavorite,
+  };
+}
